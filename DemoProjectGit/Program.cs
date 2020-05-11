fc4a96eaa86e60b9f9c1ef92243731a1494560e2
hasnaz <nazila.hasanzade@hkr.se>
2020-05-11 10:29:45 +0200

    initial commit

---
+++ DemoProjectGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoProjectGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
