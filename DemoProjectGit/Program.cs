34f8b1c92b9493102c1f5fa8b6134490596082fc
hasnaz <nazila.hasanzade@hkr.se>
2020-05-11 10:39:41 +0200

    har ändrat en console line

--- DemoProjectGit/Program.cs
+++ DemoProjectGit/Program.cs
@@ -6,7 +6,7 @@ namespace DemoProjectGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello my team!");
         }
     }
 }
